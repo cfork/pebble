287df67ae315934879bc6547c32c3660a5dca326
Cong Ding <cong@ccding.com>
2023-12-15 08:38:54 -0600

    print speed separately

--- cmd/pebble/runmy.go
+++ cmd/pebble/runmy.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"math/rand"
 	"reflect"
+	"runtime/internal/atomic"
 	"strconv"
 	"time"
 
@@ -14,6 +15,11 @@ const (
 	max                 = 1000 * 1000
 	valueSize           = 1024
 	printFrequencyInSec = 5
+)
+
+var (
+	writeCounter atomic.Int64
+	readCounter  atomic.Int64
 )
 
 var value = make([]byte, valueSize)
@@ -46,6 +52,7 @@ func main() {
 	}
 	db.Flush()
 
+	go metrics()
 	go write(db)
 
 	for i := 0; i < 5; i++ {
@@ -55,48 +62,43 @@ func main() {
 	time.Sleep(time.Hour)
 }
 
-func write(db *pebble.DB) {
-	previ := 0
+func metrics() {
 	ticker := time.NewTicker(printFrequencyInSec * time.Second)
+	for range ticker.C {
+		w := writeCounter.Swap(0)
+		r := readCounter.Swap(0)
+		log.Printf("write: %d ops/s, read: %d ops/s", w/printFrequencyInSec, r/printFrequencyInSec)
+	}
+}
+
+func write(db *pebble.DB) {
 	for i := 0; ; i++ {
-		select {
-		case <-ticker.C:
-			log.Println("write", (i-previ)/printFrequencyInSec, "ops/sec")
-			previ = i
-		default:
-			n := rand.Intn(max)
-			key := []byte(strconv.Itoa(n))
-			newValue := getValue(n)
-			if err := db.Set(key, newValue, writeOpts); err != nil {
-				log.Fatal(err)
-			}
-			time.Sleep(100 * time.Millisecond)
+		writeCounter.Add(1)
+		n := rand.Intn(max)
+		key := []byte(strconv.Itoa(n))
+		newValue := getValue(n)
+		if err := db.Set(key, newValue, writeOpts); err != nil {
+			log.Fatal(err)
 		}
+		time.Sleep(100 * time.Millisecond)
 	}
 }
 
 func read(db *pebble.DB) {
-	ticker := time.NewTicker(printFrequencyInSec * time.Second)
-	previ := 0
 	for i := 0; ; i++ {
-		select {
-		case <-ticker.C:
-			log.Println("read", (i-previ)/printFrequencyInSec, "ops/sec")
-			previ = i
-		default:
-			n := rand.Intn(max)
-			key := []byte(strconv.Itoa(n))
-			val, closer, err := db.Get(key)
-			if err != nil {
-				log.Fatal(err)
-			}
-			newValue := getValue(n)
-			if !reflect.DeepEqual(val, newValue) {
-				log.Fatalf("not equal: %d\n%s\n%s", i, string(val), string(newValue))
-			}
-			if err := closer.Close(); err != nil {
-				log.Fatal(err)
-			}
+		readCounter.Add(1)
+		n := rand.Intn(max)
+		key := []byte(strconv.Itoa(n))
+		val, closer, err := db.Get(key)
+		if err != nil {
+			log.Fatal(err)
+		}
+		newValue := getValue(n)
+		if !reflect.DeepEqual(val, newValue) {
+			log.Fatalf("not equal: %d\n%s\n%s", i, string(val), string(newValue))
+		}
+		if err := closer.Close(); err != nil {
+			log.Fatal(err)
 		}
 	}
 }
